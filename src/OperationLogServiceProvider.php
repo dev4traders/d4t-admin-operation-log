dcccdb394dca091dbfaf1fa288b701d99ec829be
mikha-dev <mikha.dev@gmail.com>
2023-12-12 12:51:53 +0300

    type

--- src/OperationLogServiceProvider.php
+++ src/OperationLogServiceProvider.php
@@ -2,11 +2,17 @@
 
 namespace Dcat\Admin\OperationLog;
 
+use Dcat\Admin\Enums\ExtensionType;
 use Dcat\Admin\Extend\ServiceProvider;
 use Dcat\Admin\OperationLog\Http\Middleware\LogOperation;
 
 class OperationLogServiceProvider extends ServiceProvider
 {
+    public function getExtensionType(): ExtensionType
+    {
+        return ExtensionType::ADDON;
+    }
+    
     protected $middleware = [
         'middle' => [
             LogOperation::class,
